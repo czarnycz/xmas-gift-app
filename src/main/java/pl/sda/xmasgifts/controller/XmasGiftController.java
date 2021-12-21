8862afb4ee8d1b36ef6519317899da105e314c1b
uzytkownik <uzytkownik@kursGit.pl>
2021-12-21 21:45:36 +0100

    init

--- src/main/java/pl/sda/xmasgifts/controller/XmasGiftController.java
+++ src/main/java/pl/sda/xmasgifts/controller/XmasGiftController.java
@@ -2,12 +2,36 @@ package pl.sda.xmasgifts.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.xmasgifts.entity.Person;
+import pl.sda.xmasgifts.service.XmasGiftService;
 
 @Controller
 public class XmasGiftController {
 
+    private final XmasGiftService xmasGiftService;
+
+    public XmasGiftController(XmasGiftService xmasGiftService) {
+        this.xmasGiftService = xmasGiftService;
+    }
+
     @GetMapping("/")
     public String home(){
         return "index";
+    }
+
+    @GetMapping("/user/add")
+    public String addUserForm(){
+        return "add-user-form";
+    }
+
+    @PostMapping("/user/add")
+    public ModelAndView addUser(@ModelAttribute Person person){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("add-user-confirm");
+        modelAndView.getModelMap().addAttribute("person",xmasGiftService.addPerson(person));
+        return modelAndView;
     }
 }
