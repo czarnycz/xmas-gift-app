c9a91fa7bd0d78a649a968a06be815d8834cc1c2
uzytkownik <uzytkownik@kursGit.pl>
2021-12-21 19:23:11 +0100

    init

---
+++ src/main/java/pl/sda/xmasgifts/service/XmasGiftService.java
@@ -0,0 +1,18 @@
+package pl.sda.xmasgifts.service;
+
+import pl.sda.xmasgifts.entity.Person;
+import pl.sda.xmasgifts.entity.Wish;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface XmasGiftService {
+    Person addPerson(Person person);
+    Optional<Wish> addPersonWish(Wish wish, long personId);
+    Wish fulfillWishByPerson(long wishId, long personId);
+    List<Wish> findAllWishes();
+    List<Person> findAllGifters();
+
+    List<Person> findAllPersons();
+}
